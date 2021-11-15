71c7ed3b03cd863cb5f814fc587ca3b7d91c217c
Edvin Alicajic <edvin.alicajic@gmail.com>
2021-11-15 21:02:52 +0100

    Added confimation when changing directories

--- src/main/scala/customGUIElements/MenuItemCustom.scala
+++ src/main/scala/customGUIElements/MenuItemCustom.scala
@@ -1,13 +1,15 @@
 package customGUIElements
 
 import scalafx.Includes._
-import scalafx.scene.control.MenuItem
+import scalafx.scene.control._
 import scalafx.event.ActionEvent
 import scalafx.stage.DirectoryChooser
 import scala.compiletime.ops.boolean
 import scala.sys.process._
 import scala.language.postfixOps
 import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.control.Alert.AlertType
+import scalafx.application.Platform
 
 class MenuItemCustom(name: String, stage: PrimaryStage) extends MenuItem {
   text = name
@@ -19,9 +21,27 @@ class MenuItemCustom(name: String, stage: PrimaryStage) extends MenuItem {
     val selectedFolder =
       directoryChooser.showDialog(stage).toString
 
-    val path = s"wslpath -w \"$selectedFolder\"".!!
-    s"explorer.exe $path".!
+    val previousFolder = ConfigReader.getProperty(s"${name}Path")
 
-    LabelHandler.update(name, path)
+    val alert = new Alert(AlertType.Confirmation) {
+      initOwner(stage)
+      title = "Confirmation Dialog"
+      headerText =
+        s"About to change $name directory from $previousFolder to $selectedFolder"
+      contentText = "Are you sure?"
+    }
+
+    val result = alert.showAndWait()
+
+    result match {
+      case Some(ButtonType.OK) => {
+        val path = s"wslpath -w \"$selectedFolder\"".!!
+        s"explorer.exe $path".!
+
+        LabelHandler.update(name, path)
+      }
+      case _ => println("Cancel or closed")
+    }
+
   }
 }
