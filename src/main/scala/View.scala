217150435d9878565a0630f23d1bfa25335cd70c
Edvin Alicajic <edvin.alicajic@gmail.com>
2021-11-15 17:10:39 +0100

    Refactored inner classes into own classes

--- src/main/scala/View.scala
+++ src/main/scala/View.scala
@@ -11,42 +11,40 @@ import scalafx.scene.paint.Color
 import scala.compiletime.ops.boolean
 import scala.sys.process._
 import scala.language.postfixOps
+import customGUIElements._
 
 object GUI extends JFXApp3 {
   override def start(): Unit = {
-    stage = new JFXApp3.PrimaryStage {
+    val fileHandler = new FileHandler(true)
+    val driveHandler = new DriveHandler
+    val dbHandler = new DBHandler(fileHandler)
 
+    stage = new JFXApp3.PrimaryStage {
       title = "Program"
       width = 1200
-
       height = 800
+
+      x = 2200
+      y = 750
+
       scene = new Scene {
         stylesheets = List(getClass.getResource("styles.css").toExternalForm)
 
-        val statusBar = new HBox {
+        val statusBar = new BorderPane {
           styleClass += "statusBar"
-          val dirLabel = new Label("Directory: None")
-          children += dirLabel
+
+          padding = new javafx.geometry.Insets(0, 5, 0, 5)
+          left = LabelHandler.dirLabel
+          right = LabelHandler.toReadLabel
         }
 
         val menuBar = new VBox {
           val tg = new ToggleGroup
           styleClass += "menuBar"
 
-          class customRadio(textArg: String, selectedArg: Boolean)
-              extends RadioButton {
-            text = textArg
-            styleClass -= "radio-button"
-            styleClass += "toggle-button"
-            minHeight = 50
-            minWidth = 100
-            toggleGroup = tg
-            selected = selectedArg
-          }
-
           children = Seq(
-            new customRadio("View", true),
-            new customRadio("Create", false),
+            new RadioCustom("View", true, tg),
+            new RadioCustom("Create", false, tg),
             new StackPane {
               alignment = Pos.BottomCenter
               vgrow = Priority.Always
@@ -55,26 +53,8 @@ object GUI extends JFXApp3 {
                 minWidth = 100
 
                 items = Seq(
-                  new MenuItem("Choose Directory") {
-                    onAction = (e: ActionEvent) => {
-                      val directoryChooser = new DirectoryChooser {
-                        title = "Choose Main Directory"
-                        initialDirectory = new java.io.File("/mnt/d/")
-                      }
-                      val selectedFolder =
-                        directoryChooser.showDialog(stage).toString
-
-                      val path = "wslpath -w \"/mnt/d/Roleplaying games\"".!!
-                      s"explorer.exe $path".!
-
-                      statusBar.getChildren.head match {
-                        case l: javafx.scene.control.Label =>
-                          l.setText(s"Directory: ${path}")
-                        case _ => println("ops")
-                      }
-
-                    }
-                  }
+                  new MenuItemCustom("Main", stage),
+                  new MenuItemCustom("ToRead", stage)
                 )
               }
             }
@@ -83,7 +63,6 @@ object GUI extends JFXApp3 {
 
         root = new BorderPane {
           bottom = statusBar
-
           left = menuBar
         }
       }
