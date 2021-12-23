777a6f6ba68e4e4039b1912277967b70fe2d2e6c
Edvin Alicajic <edvin.alicajic@gmail.com>
2021-12-23 21:05:06 +0100

    Added FolderPane and a ComboPane

--- src/main/scala/CustomGUI/CustomPane/FolderPane.scala
+++ src/main/scala/CustomGUI/CustomPane/FolderPane.scala
@@ -1,3 +1,29 @@
 import scalafx.scene.layout.TilePane
+import scalafx.scene.layout.FlowPane
+import scalafx.scene.layout.AnchorPane
+import scalafx.scene.control.Label
+import scalafx.scene.layout.BorderPane
+import scalafx.scene.input.MouseEvent
+import scalafx.Includes.eventClosureWrapperWithZeroParam
 
-class FolderPane extends TilePane {}
+class FolderPane extends FlowPane {
+  styleClass += "folder-pane"
+  prefWrapLength = 110 * 5
+
+}
+
+case class ComboPane(labelText: String, folder: FolderPane, tiles: FlowPane)
+    extends BorderPane {
+  top = new BorderPane {
+    center = new Label(labelText) {
+      styleClass += "folder-label"
+      onMouseClicked = () => {
+        folder.visible = !folder.isVisible
+        folder.managed = !folder.isManaged
+        tiles.requestLayout
+      }
+    }
+  }
+  folder.setId(labelText)
+  center = folder
+}
