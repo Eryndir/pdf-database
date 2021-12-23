0afbe7a69e7d69b512912b1c465d079369813f90
Edvin Alicajic <edvin.alicajic@gmail.com>
2021-12-23 21:04:15 +0100

    made lambda func easier

--- src/main/scala/CustomGUI/MenuItemCustom.scala
+++ src/main/scala/CustomGUI/MenuItemCustom.scala
@@ -9,10 +9,12 @@ import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.control.Alert.AlertType
 import scalafx.application.Platform
 import FilePathHandler._
+import scalafx.Includes.eventClosureWrapperWithZeroParam
+import os.Path
 
 class MenuItemCustom(name: String, stage: PrimaryStage) extends MenuItem {
   text = name
-  onAction = (e: ActionEvent) => {
+  onAction = () => {
     val directoryChooser = new DirectoryChooser {
       title = s"Choose $name Directory"
       initialDirectory = new java.io.File("/mnt/d/")
