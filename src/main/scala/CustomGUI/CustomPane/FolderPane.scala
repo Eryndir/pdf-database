0d65194b218aa16127b7acce76ec732e59fdf9d7
Edvin Alicajic <edvin.alicajic@gmail.com>
2021-12-26 21:16:41 +0100

    Refactored out inner class

--- src/main/scala/CustomGUI/CustomPane/FolderPane.scala
+++ src/main/scala/CustomGUI/CustomPane/FolderPane.scala
@@ -6,28 +6,10 @@ import scalafx.scene.layout.BorderPane
 import scalafx.scene.input.MouseEvent
 import scalafx.Includes.eventClosureWrapperWithZeroParam
 import scalafx.scene.layout.Pane
+import scalafx.geometry.Pos
 
 case class FolderPane(parentFolder: Pane = null) extends FlowPane {
   styleClass += "folder-pane"
   prefWrapLength = 110 * 5
   vgap = 10
-
-}
-
-case class ComboPane(labelText: String, folder: FolderPane, tiles: FlowPane)
-    extends BorderPane {
-  folder.visible = false
-  folder.managed = false
-  top = new BorderPane {
-    center = new Label(labelText) {
-      styleClass += "folder-label"
-      onMouseClicked = () => {
-        folder.visible = !folder.isVisible
-        folder.managed = !folder.isManaged
-        tiles.requestLayout
-      }
-    }
-  }
-  folder.setId(labelText)
-  center = folder
 }
